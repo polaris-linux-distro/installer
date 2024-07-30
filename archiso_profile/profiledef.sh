5471d0d690206755eddc276841236e030b5a3eb6
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-07-30 17:17:48 -0400

    fix

--- archiso_profile/profiledef.sh
+++ archiso_profile/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="polarislinux"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_publisher="Polaris Linux <https://polaris-linux-distro.github.io>"
+iso_application="Polaris Linux Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
