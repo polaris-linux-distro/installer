e716eb43da3daf39928fafdf42a8211f4075c61b
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-08-02 11:48:52 -0400

    fallback initramfs

--- src/main.py
+++ src/main.py
@@ -113,6 +113,18 @@ Exec = /usr/bin/python /usr/share/polaris/polo-adm.py rebuild-boot
 			f'MODULE_PATH=boot:///initramfs-linux-zen.img',
 			f'CMDLINE={kernel_params} loglevel=7 debug earlyprintk=vga plymouth.enable=0 systemd.log_level=debug systemd.log_target=console',
 		]
+		entry_normal_fallback = [
+			f'PROTOCOL=linux',
+			f'KERNEL_PATH=boot:///vmlinuz-linux-zen',
+			f'MODULE_PATH=boot:///initramfs-linux-zen-fallback.img',
+			f'CMDLINE={kernel_params} quiet splash',
+		]
+		entry_verbose_fallback = [
+			f'PROTOCOL=linux',
+			f'KERNEL_PATH=boot:///vmlinuz-linux-zen',
+			f'MODULE_PATH=boot:///initramfs-linux-zen-fallback.img',
+			f'CMDLINE={kernel_params} loglevel=7 debug earlyprintk=vga plymouth.enable=0 systemd.log_level=debug systemd.log_target=console',
+		]
 		entry_rescue = [
 			f'PROTOCOL=linux',
 			f'KERNEL_PATH=boot:///vmlinuz-linux-zen',
@@ -123,6 +135,10 @@ Exec = /usr/bin/python /usr/share/polaris/polo-adm.py rebuild-boot
 		config_contents += f'\n:Polaris Linux\n'
 		config_contents += '\n'.join([f'    {it}' for it in entry_normal]) + '\n'
 		config_contents += f'\n:Polaris Linux (Verbose mode)\n'
+		config_contents += '\n'.join([f'    {it}' for it in entry_verbose]) + '\n'
+		config_contents += f'\n:Polaris Linux (fallback initramfs)\n'
+		config_contents += '\n'.join([f'    {it}' for it in entry_normal]) + '\n'
+		config_contents += f'\n:Polaris Linux (Verbose mode,fallback initramfs)\n'
 		config_contents += '\n'.join([f'    {it}' for it in entry_verbose]) + '\n'
 		config_contents += f'\n:Polaris Linux (Emergency mode)\n'
 		config_contents += '\n'.join([f'    {it}' for it in entry_rescue]) + '\n'
